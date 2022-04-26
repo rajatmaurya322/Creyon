4a97228c7dcc08c240caf2c3c22c4b1a2dd8c3ec
blades <rajatmaurya1505@gmail.com>
2022-04-26 17:28:48 +0530

    created windowmanage which handles glfw callback functions

---
+++ Creyon/src/Render/windowmanage.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+namespace Creyon {
+
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+	void processInput(GLFWwindow* window);
+
+}
