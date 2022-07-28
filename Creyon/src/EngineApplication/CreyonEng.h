3ed4460119d2a6b6e8d0088bf94f76407d5ce92a
blades <rajatmaurya1505@gmail.com>
2022-07-28 16:59:34 +0530

    Moved from project Sandbox to Creyon

---
+++ Creyon/src/EngineApplication/CreyonEng.h
@@ -0,0 +1,30 @@
+#pragma once
+
+//All includes and aliases for Creyon engine are stored here
+
+#include "MathEngine/matrix2x2.h"
+#include "MathEngine/matrix3x3.h"
+#include "MathEngine/matrix2x2.h"
+#include "MathEngine/vector4d.h"
+#include "MathEngine/vector3d.h"
+#include "MathEngine/vector2d.h"
+#include "MathEngine/transforms.h"
+#include "MathEngine/Constants/constants.h"
+#include "MathEngine/Miscellaneous/Misc.h"
+#include "Render/windowmanage.h"
+#include "Render/Shaderprogram.h"
+#include "Render/Shader.h"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+using mat44 = Creyon::matrix_4x4;
+using mat33 = Creyon::matrix_3x3;
+using mat22 = Creyon::matrix_2x2;
+using std::cout;
+using std::cin;
+using Creyon::CreyonWindow;
+using Creyon::vector3d;
+using Creyon::vector4d;
+using Creyon::Shader;
+using Creyon::Shaderprogram;
