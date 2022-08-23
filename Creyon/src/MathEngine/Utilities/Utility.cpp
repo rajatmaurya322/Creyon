37ebc8b7639f21aa8ae62a0670c18f19395ad7e3
blades <rajatmaurya1505@gmail.com>
2022-08-24 00:02:56 +0530

    Add degree to radians conversion

---
+++ Creyon/src/MathEngine/Utilities/Utility.cpp
@@ -0,0 +1,23 @@
+#include "Utility.h"
+
+namespace Creyon {
+
+	bool abscompare(float a, float b, float abs_epsilon) {
+		//If absolute difference between a and b is less than or equal to epsilon value, they are said to be equal
+		return std::fabs(a - b) <= abs_epsilon;
+	}
+
+	bool relcompare(float a, float b, float rel_epsilon) {
+		//If difference between a and b is within epsilon percent of larger of a and b, they are said to be equal
+		return ( std::fabs(a - b) <= std::fmax( std::fabs(a), std::fabs(b) ) * rel_epsilon );
+	}
+
+	//Conversion to 4x4 matrix
+	matrix_4x4 convert_mat4(const matrix_3x3& mat3, const vector3d& vec3) {
+		return matrix_4x4(mat3.m_elems[0], mat3.m_elems[1], mat3.m_elems[2], 0.0f,
+						  mat3.m_elems[3], mat3.m_elems[4], mat3.m_elems[5], 0.0f,
+						  mat3.m_elems[6], mat3.m_elems[7], mat3.m_elems[8], 0.0f,
+						  vec3.m_x		 , vec3.m_y		  , vec3.m_z	   , 1.0f);
+	}
+
+}
---
+++ Creyon/src/MathEngine/Utilities/Utility.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "MathEngine/matrix3x3.h"
+#include "MathEngine/matrix4x4.h"
+#include "MathEngine/vector3d.h"
+#include "MathEngine/vector2d.h"
+#include "MathEngine/Constants/constants.h"
+#include <cmath>
+
+namespace Creyon {
+
+	//Epsilon checks for floating-point comparisons
+
+	//Absolute epsilon comparison
+	bool abscompare(float a, float b, float abs_epsilon);
+
+	//Relative epsilon comparison
+	bool relcompare(float a, float b, float rel_epsilon);
+
+	//Square function
+	inline float square(const float value) {
+		return value * value;
+	}
+
+	inline float degToRad(const float angleInDeg) {
+		return angleInDeg * degtorad;
+	}
+	
+	matrix_4x4 convert_mat4(const matrix_3x3& mat3, const vector3d& vec3);
+}
