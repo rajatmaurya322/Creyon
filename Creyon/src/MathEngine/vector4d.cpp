b8d304697ec952833df800e97b47f166358789c6
blades <rajatmaurya1505@gmail.com>
2022-08-29 14:34:29 +0530

    Create vector4d.cpp and add vector4d definitions

---
+++ Creyon/src/MathEngine/vector4d.cpp
@@ -0,0 +1,20 @@
+#include "vector4d.h"
+
+//Namespace is Creyon
+namespace Creyon {
+
+	//Overloaded operators
+	//Vector matrix multiplication; v*mat form
+	vector4d vector4d::operator*(const Creyon::matrix_4x4& mat) {
+		return vector4d(m_x * mat.m_elems[0] + m_y * mat.m_elems[4] + m_z * mat.m_elems[8] + m_w * mat.m_elems[12],
+			m_x * mat.m_elems[1] + m_y * mat.m_elems[5] + m_z * mat.m_elems[9] + m_w * mat.m_elems[13],
+			m_x * mat.m_elems[2] + m_y * mat.m_elems[6] + m_z * mat.m_elems[10] + m_w * mat.m_elems[14],
+			m_x * mat.m_elems[3] + m_y * mat.m_elems[7] + m_z * mat.m_elems[11] + m_w * mat.m_elems[15]);
+	}
+
+	//overloaded << operator to display vector directly with cout
+	std::ostream& operator<<(std::ostream& os, const vector4d& vec) {
+		os << "X:" << vec.m_x << "\nY:" << vec.m_y << "\nZ:" << vec.m_z << "\nW:" << vec.m_w;
+		return os;
+	}
+}
