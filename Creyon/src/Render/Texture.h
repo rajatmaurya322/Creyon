a68445d9e2b7f37482829c67261ef8d95ca3018c
blades <rajatmaurya1505@gmail.com>
2022-09-02 23:31:53 +0530

    Create new Texture class

---
+++ Creyon/src/Render/Texture.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "glad/glad.h"
+#include "stb_image.h"
+#include "Utilities/Utility.h"
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+//Namespace is Creyon
+namespace Creyon {
+	
+	//Class representing a texture object
+	class Texture{
+	private:
+		unsigned int texId;
+
+	public:
+
+		enum class Format { JPG, PNG };
+		
+		//Generates a single texture and sets it as target
+		Texture();
+
+		//Sets the calling texture object as target texture
+		void setTexAsTarget();
+
+		//Get the texture Id
+		inline unsigned int getTexId() {
+			return texId;
+		}
+
+		//Configures texture wrapping methods
+		void configTexWrap(GLint mode_Xaxis, GLint mode_Yaxis);
+
+		//Configures texture filtering modes
+		void configTexFilters(GLenum filter, GLint mode);
+
+		//Loads an image into the texture object
+		void loadImg(const char* pathtoimg, Format imgformat);
+		
+		//Sets the texture unit location for texture object
+		void setTexUnit(GLenum TextureUnit);
+	};
+}
+
