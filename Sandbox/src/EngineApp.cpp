9b0d18a36852f0eb420014da6b4dcba8e4543aba
blades <rajatmaurya1505@gmail.com>
2022-09-02 23:27:59 +0530

    Modified project to use new Texture class

--- Sandbox/src/EngineApp.cpp
+++ Sandbox/src/EngineApp.cpp
@@ -87,49 +87,25 @@ namespace Creyon {
 		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 		glEnableVertexAttribArray(1);
 
-		unsigned int texture, texture2;
-		glGenTextures(1, &texture);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		
-
-		//sampling texture colors, filtering options
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		
-		int width, height, nrchannels;
-		stbi_set_flip_vertically_on_load(true);
-		unsigned char* data = stbi_load("GOW.jpg", &width, &height, &nrchannels, 0);
-		if(data){
-			//Creating textures
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-			glGenerateMipmap(GL_TEXTURE_2D);
-		}
-		else {
-			std::cout << "\nERROR: Failed to load texture";
-		}
-		stbi_image_free(data);
-
-		glGenTextures(1, &texture2);
-		glBindTexture(GL_TEXTURE_2D, texture2);
+		Texture tex1;
 		
 		//sampling texture colors, filtering options
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		tex1.configTexWrap(GL_REPEAT, GL_REPEAT);
+		
+		tex1.configTexFilters(GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		tex1.configTexFilters(GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		
+		tex1.loadImg("GOW.jpg", Texture::Format::JPG);
 
-		data = stbi_load("awesomeface.png", &width, &height, &nrchannels, 0);
-		if (data) {
-			//Creating textures
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-			glGenerateMipmap(GL_TEXTURE_2D);
-		}
-		else {
-			std::cout << "\nERROR: Failed to load texture";
-		}
-		stbi_image_free(data);
+		Texture tex2;
+		
+		//sampling texture colors, filtering options
+		tex2.configTexWrap(GL_REPEAT, GL_REPEAT);
+
+		tex2.configTexFilters(GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		tex2.configTexFilters(GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		tex2.loadImg("awesomeface.png", Texture::Format::PNG);
 
 		//unbind vao
 		glBindVertexArray(0);
@@ -145,10 +121,8 @@ namespace Creyon {
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-			glActiveTexture(GL_TEXTURE0);
-			glBindTexture(GL_TEXTURE_2D, texture);
-			glActiveTexture(GL_TEXTURE1);
-			glBindTexture(GL_TEXTURE_2D, texture2);
+			tex1.setTexUnit(GL_TEXTURE0);
+			tex2.setTexUnit(GL_TEXTURE1);
 
 			programrect.useProgram();
 			glUniform1i(glGetUniformLocation(programrect.getId(), "ourTexture"), 0);
@@ -157,8 +131,8 @@ namespace Creyon {
 			unsigned int transformloc = glGetUniformLocation(programrect.getId(), "transform");
 			
 			mat44 trans;
-			mat44 model = /* rotateX((float)glfwGetTime(), false) */ rotateY((float)glfwGetTime(), false);
-			mat44 view = translate(0.0f, 0.0f, -4.0f);
+			mat44 model = scale( (sinf( (float)glfwGetTime() )/2.0f + 0.5f)* 2) * rotateY((float)glfwGetTime(), false) * rotateX((float)glfwGetTime(), false);
+			mat44 view = translate(0.0f, 0.0f, -5.0f);
 			mat44 proj = persp(800.0f / 600.0f, pi_u4, 100.0f, 0.1f);
 			trans = trans  * model * view * proj;
 			glUniformMatrix4fv(transformloc, 1, GL_TRUE, trans.m_elems);
