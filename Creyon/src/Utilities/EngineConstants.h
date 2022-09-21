c42cd0955469eeb73f69529bb0b2726c1010700f
blades <rajatmaurya1505@gmail.com>
2022-09-21 22:01:21 +0530

    Add EngineConstants.h which depends on Engine

---
+++ Creyon/src/Utilities/EngineConstants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Utility.h"
+
+namespace Creyon {
+
+	const std::filesystem::path EngineRootPath = searchRootDir();
+
+	const std::filesystem::path CreyonSrcPath = EngineRootPath / "Creyon\\src";
+}
