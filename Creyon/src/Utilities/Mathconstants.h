86424837099e38439cd7643c9710da99a47d9f88
blades <rajatmaurya1505@gmail.com>
2022-09-21 22:02:30 +0530

    Rename constants.h to Mathconstants.h and move to Utilities

---
+++ Creyon/src/Utilities/Mathconstants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//Contains constants not dependent on engine
+namespace Creyon {
+	
+	const float pi		=	3.141592653589793f;
+
+	const float pi_u2	=	1.570796326794896f;
+
+	const float pi_u4	=	0.785398163397448f;
+
+	const float degtorad=	0.0174532925199f; 
+}
