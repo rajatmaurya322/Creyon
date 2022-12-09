1d9b90fa517fd4a487e4a59164896a760c176a07
blades <rajatmaurya1505@gmail.com>
2022-12-09 21:35:20 +0530

    Remove CreyonWindow member

--- Sandbox/src/EngineApp.h
+++ Sandbox/src/EngineApp.h
@@ -7,13 +7,8 @@ namespace Creyon {
 	//Class for representing the application entry point
 	class EngineApp
 	{	
-		private:
-			CreyonWindow windowInstance;
 		public:
-		EngineApp();
-
 		void Run();
-
 	};
 
 }
