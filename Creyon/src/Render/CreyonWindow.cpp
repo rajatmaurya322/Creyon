02bc1d39e86e1ce10b032b5e768891f0148d2b58
blades <rajatmaurya1505@gmail.com>
2022-12-09 21:32:13 +0530

    Add mouse and keyboard callbacks

--- Creyon/src/Render/CreyonWindow.cpp
+++ Creyon/src/Render/CreyonWindow.cpp
@@ -2,18 +2,17 @@
 
 namespace Creyon {
 
-	CreyonWindow::CreyonWindow():m_width{600}, m_height{600}, mp_title{"New Window"}
-	{
-		//Create a window with default width(600), height(600) and title(New Window)
-		mp_window = glfwCreateWindow(m_width, m_height, mp_title, nullptr, nullptr);
-	}
-	
 	CreyonWindow::CreyonWindow(int width, int height, const char* title) 
 	:m_width{ width }, m_height{ height }, mp_title{ title }
 	{
 		//Create a window with given width, height and title
 		mp_window = glfwCreateWindow(m_width, m_height, mp_title, nullptr, nullptr);
-		
+		m_lastFrame = 0.0f;
+		m_lastX = m_width / 2.0f;
+		m_lastY = m_height / 2.0f;
+		m_firstMouse = true;
+		mp_cam = nullptr;
+
 		if (mp_window == nullptr) {
 			std::cout << "Failed to create GLFW window\n";
 			glfwTerminate(); //Terminate glfw
@@ -22,7 +21,7 @@ namespace Creyon {
 		setContextCurrent(); //Set window context as current
 	}
 
-	void CreyonWindow::CreyonWindowInit() {
+	void CreyonWindow::Init() {
 		glfwInit();  //Initialise glfw
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);  //Set Opengl version to 4.3 
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -33,29 +32,74 @@ namespace Creyon {
 		glfwMakeContextCurrent(mp_window);
 	}
 
-	void CreyonWindow::register_Callback(){
-		//Sets the user pointer to be retrieved back in the callback function
-		glfwSetWindowUserPointer(mp_window, reinterpret_cast<void*>(this));  
+	void CreyonWindow::activateCamera(Camera& pcam) {
+		//Takes the camera object and gives its address to the pointer
+		mp_cam = &pcam;
+	}
 
+	void CreyonWindow::reg_ResizeCallback(){
+		//Sets the user pointer to be retrieved back in the callback function
+		glfwSetWindowUserPointer(mp_window, reinterpret_cast<void*>(this));
 		glfwSetFramebufferSizeCallback(mp_window, framebuffer_size_callback);
 	}
 
-	void CreyonWindow::processInput() {
-		if (glfwGetKey(mp_window, GLFW_KEY_ESCAPE) == GLFW_PRESS) { //If ESC key is pressed
-			glfwSetWindowShouldClose(mp_window, true); //Close the window
-		}
+	void CreyonWindow::reg_MouseCallback() {
+		glfwSetWindowUserPointer(mp_window, reinterpret_cast<void*>(this));
+		glfwSetCursorPosCallback(mp_window, mouse_callback);
+	}
+
+	void CreyonWindow::closeWindow() {
+		glfwSetWindowShouldClose(mp_window, true); //Close the window
+	}
+
+	bool CreyonWindow::keyPress(int key) {
+		return glfwGetKey(mp_window, key) == GLFW_PRESS;
+	}
+
+	void CreyonWindow::processKeyboard() {
+		float dT = deltaTime();
+		float camSpeed = mp_cam->getSpeed() * dT;
+		//Handle inputs related to the camera here
+		if (keyPress(GLFW_KEY_ESCAPE))
+			closeWindow();
+		if (keyPress(GLFW_KEY_W))
+			mp_cam->moveForward(camSpeed);
+		if (keyPress(GLFW_KEY_S))
+			mp_cam->moveBackward(camSpeed);
+		if (keyPress(GLFW_KEY_A))
+			mp_cam->moveLeft(camSpeed);
+		if (keyPress(GLFW_KEY_D))
+			mp_cam->moveRight(camSpeed);
+	}
+
+	void CreyonWindow::processMouse(float offsetX, float offsetY) {
+		mp_cam->increaseAngle(offsetX, offsetY);
+		mp_cam->rotateCamera();
+	}
+
+	float CreyonWindow::deltaTime() {
+		//Calculating time taken to render previous frame
+		float current = CreyonWindow::getTime();
+		float delta = current - m_lastFrame;
+		//updates lastFrame value
+		m_lastFrame = current;
+		return delta;
 	}
 
 	void CreyonWindow::setSize(const int nwidth, const int nheight) {
 		glfwSetWindowSize(mp_window, nwidth, nheight);  //Sets window size to new width and height
 	}
+
+	void CreyonWindow::setInputMode(int mode, int value) {
+		glfwSetInputMode(mp_window, mode, value);
+	}
  
-	void CreyonWindow::CreyonWindowTerminate(CreyonWindow& window) {
+	void CreyonWindow::Terminate(CreyonWindow& window) {
 		
 		glfwDestroyWindow(window.mp_window); //Destroy the window
 		window.mp_window = nullptr;  //Set object's pointers to null
 		window.mp_title = nullptr;
-		
+		window.mp_cam = nullptr;
 		window.m_width = window.m_height = 0; //Set height and width to 0
 		
 		glfwTerminate(); //Terminate glfw
@@ -71,5 +115,33 @@ namespace Creyon {
 		self->setHeight(height);
 
 		glViewport(0, 0, width, height); //Resize viewport to new width and height
+	}
+
+	void CreyonWindow::mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+		
+		CreyonWindow* self = reinterpret_cast<CreyonWindow*>(glfwGetWindowUserPointer(window));
+
+		float MouseXPos = static_cast<float>(xpos);
+		float MouseYPos = static_cast<float>(ypos);
+
+		//If first mouse movement, then prevent jumps by setting it to current position
+		if (self->m_firstMouse) {
+			self->m_lastX = MouseXPos;
+			self->m_lastY = MouseYPos;
+			self->m_firstMouse = false;
+		}
+
+		//Set the mouse sensitivity
+		const float sensitivity = 0.5f;
+		
+		//Calculates the offset from mouse last position
+		float offsetX = (MouseXPos - self->m_lastX)*sensitivity;
+		float offsetY = (self->m_lastY - MouseYPos)*sensitivity;
+
+		//Updates mouse last position to current position
+		self->m_lastX = xpos;
+		self->m_lastY = ypos;
+
+		self->processMouse(offsetX, offsetY);
 	}
 }
--- Creyon/src/Render/CreyonWindow.h
+++ Creyon/src/Render/CreyonWindow.h
@@ -2,6 +2,7 @@
 
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "Camera.h"
 #include <iostream>
 
 namespace Creyon {
@@ -9,16 +10,15 @@ namespace Creyon {
 	//This class abstracts over GLFW Window creation for ease of use
 	class CreyonWindow
 	{
-
 	private:
 		GLFWwindow* mp_window;	//prefix mp_ stands for member of class and is a pointer
-		int m_width, m_height;
 		const char* mp_title;
+		int m_width, m_height;
+		float m_lastFrame, m_lastX, m_lastY;
+		bool m_firstMouse;
+		Camera* mp_cam;
 
 	public:
-
-		//creates a default glfw window
-		CreyonWindow();
 
 		//creates a glfw window with given parameters
 		CreyonWindow(int width, int height, const char* title);
@@ -28,29 +28,53 @@ namespace Creyon {
 		inline int getHeight(){ return m_height; }
 		
 		//Set methods
-		inline void setWidth(const int nwidth) { m_width = nwidth ;}
-		inline void setHeight(const int nheight) {m_height = nheight ;}
+		inline void setWidth(const int width) { m_width = width ;}
+		inline void setHeight(const int height) {m_height = height ;}
 		
 		//Properly initialises glfw
-		static void CreyonWindowInit();
+		static void Init();
 		
 		//Sets context of the window object's window to current
 		void setContextCurrent();
 
-		//Registers the given window for resizing callback
-		void register_Callback();
+		//Activates the Camera
+		void activateCamera(Camera& pcam);
 
-		//Processes the keyboard input events
-		void processInput();
+		//Registers the given window for resizing callback(reg is short for register)
+		void reg_ResizeCallback();
+
+		//Registers the given window for mouse callback
+		void reg_MouseCallback();
+
+		//Closes a window
+		void closeWindow();
+
+		//Detect a KeyPress
+		bool keyPress(int key);
+
+		//Processes all keyboard based input
+		void processKeyboard();
+
+		//Processes all mouse based input
+		void processMouse(float offsetX, float offsetY);
+
+		//Returns DeltaTime
+		float deltaTime();
 
 		//Resizes the window to new height and width
 		void setSize(const int nwidth,const int nheight);
+
+		//Sets the input mode for a window
+		void setInputMode(int mode, int value);
 		
 		//callback function which resizes the framebuffer as the window size changes
 		static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+		//callback function which calculates mouse offsets
+		static void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 		
 		// Properly terminates glfw window
-		static void CreyonWindowTerminate(CreyonWindow& window);
+		static void Terminate(CreyonWindow& window);
 
 		//Swap window buffers
 		inline void swapBuffers() {
