7d1e16d928fb8615216655e84a353f1bab517513
blades <rajatmaurya1505@gmail.com>
2022-12-09 21:29:34 +0530

    Add model, view and projection uniforms

--- Creyon/src/Render/VertexShader.glsl
+++ Creyon/src/Render/VertexShader.glsl
@@ -4,10 +4,12 @@ layout (location = 1) in vec2 atexturecoord;
 
 out vec2 textCoord;
 
-uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-gl_Position= vec4(aPos, 1.0) * transform;
+gl_Position= vec4(aPos, 1.0) * model* view * projection;
 textCoord = atexturecoord;
 }
