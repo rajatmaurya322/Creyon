1d31ca756558e8d730f0ab9c2de57130b6a37841
blades <rajatmaurya1505@gmail.com>
2023-02-11 22:54:17 +0530

    Rename quaternion class to Quaternion

--- Creyon/src/MathEngine/quaternion.cpp
+++ Creyon/src/MathEngine/quaternion.cpp
@@ -2,51 +2,51 @@
 
 namespace Creyon {
 
-	quaternion::quaternion():m_w{0.0f}, m_x{0.0f}, m_y{0.0}, m_z{0.0f}
+	Quaternion::Quaternion():m_w{0.0f}, m_x{0.0f}, m_y{0.0}, m_z{0.0f}
 	{}
 
-	quaternion::quaternion(const float w, const float x, const float y, const float z):m_w{w}, m_x{x}, m_y{y}, m_z{z}
+	Quaternion::Quaternion(const float w, const float x, const float y, const float z):m_w{w}, m_x{x}, m_y{y}, m_z{z}
 	{}
 
-	quaternion quaternion::operator+(const quaternion& quat)const {
+	Quaternion Quaternion::operator+(const Quaternion& quat)const {
 		//Adds respective components
-		return quaternion(m_w + quat.m_w, m_x + quat.m_x, m_y + quat.m_y, m_z + quat.m_z);
+		return Quaternion(m_w + quat.m_w, m_x + quat.m_x, m_y + quat.m_y, m_z + quat.m_z);
 	}
 
-	quaternion quaternion::operator-(const quaternion& quat)const {
+	Quaternion Quaternion::operator-(const Quaternion& quat)const {
 		//Subtracts respective components
-		return quaternion(m_w - quat.m_w, m_x - quat.m_x, m_y - quat.m_y, m_z - quat.m_z);
+		return Quaternion(m_w - quat.m_w, m_x - quat.m_x, m_y - quat.m_y, m_z - quat.m_z);
 	}
 
-	quaternion quaternion::operator*(const quaternion& quat)const {
+	Quaternion Quaternion::operator*(const Quaternion& quat)const {
 		//Quaternion multiplication
-		return quaternion(m_w * quat.m_w -  m_x * quat.m_x -  m_y * quat.m_y -  m_z * quat.m_z,
+		return Quaternion(m_w * quat.m_w -  m_x * quat.m_x -  m_y * quat.m_y -  m_z * quat.m_z,
 						  m_w * quat.m_x +  quat.m_w * m_x +  m_y * quat.m_z -  quat.m_y * m_z,
 						  m_w * quat.m_y +  quat.m_w * m_y +  m_z * quat.m_x -  quat.m_z * m_x, 
 						  m_w * quat.m_z +  quat.m_w * m_z +  m_x * quat.m_y -  quat.m_x * m_y );
 	}
 
-	quaternion quaternion::operator*(const float& s)const {
+	Quaternion Quaternion::operator*(const float& s)const {
 		//scalar multiplication
-		return quaternion(m_w * s, m_x * s, m_y * s, m_z * s);
+		return Quaternion(m_w * s, m_x * s, m_y * s, m_z * s);
 	}
 
-	quaternion quaternion::operator/(const float& s)const {
+	Quaternion Quaternion::operator/(const float& s)const {
 		if (s == 0) { //Can't divide by 0
 			std::cout << "Quaternion division by 0 error";
 			exit(EXIT_FAILURE);
 		}
 		else {
 			//scalar division
-			return quaternion(m_w / s, m_x / s, m_y / s, m_z / s);
+			return Quaternion(m_w / s, m_x / s, m_y / s, m_z / s);
 		}
 	}
 
-	quaternion quaternion::operator-()const {
-		return quaternion(-m_w, -m_x, -m_y, -m_z);
+	Quaternion Quaternion::operator-()const {
+		return Quaternion(-m_w, -m_x, -m_y, -m_z);
 	}
 
-	std::ostream& operator<<(std::ostream& os, const quaternion& quat) {
+	std::ostream& operator<<(std::ostream& os, const Quaternion& quat) {
 		//prints respective components
 		os << "\nw:" << quat.m_w << "\tx:" << quat.m_x << "\ty:" << quat.m_y << "\tz:" << quat.m_z;
 		return os;
@@ -54,32 +54,32 @@ namespace Creyon {
 	
 	//Non-member functions
 	
-	float sq_magn(const quaternion& q) {
+	float sq_magn(const Quaternion& q) {
 		//Square of magnitude: w^2 + x^2+ y^2 + z^2 
 		return q.m_w * q.m_w + q.m_x * q.m_x + q.m_y * q.m_y + q.m_z * q.m_z;
 	}
 
-	float magn(const quaternion& q) {
+	float magn(const Quaternion& q) {
 		//Magnitude: sqrt(w^2 + x^2+ y^2 + z^2)
 		return sqrt(sq_magn(q));
 	}
 
-	void normalize(quaternion& q) {
+	void normalize(Quaternion& q) {
 		q = q / magn(q);
 	}
 
-	quaternion inverse(const quaternion& q) {
+	Quaternion inverse(const Quaternion& q) {
 		//Quaternion Inverse: conjugate(q) / |q|^2
-		return quaternion(q.m_w, -q.m_x, -q.m_y, -q.m_z) / sq_magn(q);
+		return Quaternion(q.m_w, -q.m_x, -q.m_y, -q.m_z) / sq_magn(q);
 	}
 
-	quaternion slerp(const quaternion& start, const quaternion& end, const float t) {
+	Quaternion slerp(const Quaternion& start, const Quaternion& end, const float t) {
 		
 		//calculates cosine of angle between quaternions
 		float cosOmega = start.m_w * end.m_w + start.m_x * end.m_x + start.m_y * end.m_y + start.m_z * end.m_z;
 
 		//For negative dot product, negate the end quaternion to take shorter arc
-		quaternion neg_end = end;
+		Quaternion neg_end = end;
 		if (cosOmega < 0.0f) {
 			neg_end = -end;
 			cosOmega = -cosOmega;
--- Creyon/src/MathEngine/quaternion.h
+++ Creyon/src/MathEngine/quaternion.h
@@ -6,54 +6,53 @@
 //Namespace is Creyon
 namespace Creyon {
 	
-	class quaternion
-	{
+	class Quaternion{
 	public: //All data members begin with m_ prefix
 
 		float m_w, m_x, m_y, m_z; // represents quaternion form: W + Xi + Yj + Zk
 		
 		//Creates a null quaternion
-		quaternion();
+		Quaternion();
 
 		//Creates a quaternion based on given values
-		quaternion(const float w, const float x, const float y , const float z);
+		Quaternion(const float w, const float x, const float y , const float z);
 
 		//Adds two quaternions
-		quaternion operator+(const quaternion& quat)const;
+		Quaternion operator+(const Quaternion& quat)const;
 		
 		//Subtracts two quaternions
-		quaternion operator-(const quaternion& quat)const;
+		Quaternion operator-(const Quaternion& quat)const;
 
 		//Multiplies two quaternions
-		quaternion operator*(const quaternion& quat)const;
+		Quaternion operator*(const Quaternion& quat)const;
 
 		//Multiplies quaternion by scalar: q * s form
-		quaternion operator*(const float& s)const;
+		Quaternion operator*(const float& s)const;
 
 		//Divides quaternion by scalar
-		quaternion operator/(const float& s)const;
+		Quaternion operator/(const float& s)const;
 
 		//unary - (negates all components)
-		quaternion operator-()const;
+		Quaternion operator-()const;
 
 		//Prints quaternion directly with cout
-		friend std::ostream& operator<<(std::ostream& os, const quaternion& quat);
+		friend std::ostream& operator<<(std::ostream& os, const Quaternion& quat);
 	};
 
 	//Non member functions
 
 	//Returns square of magnitude of quaternion
-	float sq_magn(const quaternion& q);
+	float sq_magn(const Quaternion& q);
 
 	//Returns magnitude of quaternion
-	float magn(const quaternion& q);
+	float magn(const Quaternion& q);
 
 	//Normalize a quaternion
-	void normalize(quaternion& q);
+	void normalize(Quaternion& q);
 
 	//Returns inverse of quaternion
-	quaternion inverse(const quaternion& q);
+	Quaternion inverse(const Quaternion& q);
 
 	//interpolates two quaternions -aka- Slerp(Spherical Linear Interpolation)
-	quaternion slerp(const quaternion& start, const quaternion& end, const float t);
+	Quaternion slerp(const Quaternion& start, const Quaternion& end, const float t);
 }
