27ae76a1e796f9f4ff9aab83dfcdea9c4b8fee99
blades <rajatmaurya1505@gmail.com>
2023-04-01 21:19:14 +0530

    Camera now derives from Entity class

--- Creyon/src/Render/Camera.cpp
+++ Creyon/src/Render/Camera.cpp
@@ -2,25 +2,18 @@
 
 namespace Creyon {
 
-	Camera::Camera() {
-		//Set camera to these default vectors
-		vector3d camPos{ 0.0f,0.0f,3.0f }, camFront{ 0.0f,0.0f,-1.0f }, camUp{ 0.0f,1.0f,0.0f };
-		setCamera(camPos, camFront, camUp, 3.0f);
-	}
-
-	void Camera::setCamera(const vector3d& pos, const vector3d& front, const vector3d& up, const float speed)
-	{
-		m_pos = pos; m_front = front; m_target = m_pos + m_front;
-		m_up = up; m_speed = speed;
-		m_yaw = -90.0f; m_pitch = 0.0f;
-	}
-
-	void Camera::UpdateTarget() {
-		m_target = m_pos + m_front;
-	}
+	Camera::Camera()
+	:m_pos{ 0.0f,0.0f,3.0f },
+	 m_front{ 0.0f,0.0f,-1.0f },
+	 m_up{ 0.0f,1.0f,0.0f },
+	 m_yaw{ -90.0f },
+	 m_pitch{ 0.0f },
+	 m_speed{3.0f},
+	 m_target{ m_pos + m_front }
+	{}
 
 	Mat44 Camera::lookAt(){
-		UpdateTarget();
+		m_target = m_pos + m_front; //Updates the camera target
 		vector3d direction = normalize(m_pos - m_target);
 		vector3d right = normalize(cross(m_up, direction));
 		vector3d up = cross(direction, right);
@@ -29,47 +22,45 @@ namespace Creyon {
 		Mat44 LookAt{	right.m_x,		up.m_x,		direction.m_x,		0.0f,
 						right.m_y,		up.m_y,		direction.m_y,		0.0f,
 						right.m_z,		up.m_z,		direction.m_z,		0.0f,
-					  -(right*m_pos), -(up*m_pos), -(direction*m_pos),1.0f };
+					  -(right*m_pos), -(up*m_pos), -(direction*m_pos),  1.0f };
 		
 		return LookAt;
 	}
 
-	void Camera::moveForward(const float camSpeed) {
-		m_pos = m_pos + camSpeed * m_front;
-	}
+	void Camera::onKey() {
+		float camspeed = m_speed * getDelta() ;
 
-	void Camera::moveBackward(const float camSpeed) {
-		m_pos = m_pos - camSpeed * m_front;
+		if (isPressed(GLFW_KEY_W)) {
+			m_pos = m_pos + camspeed * m_front;		//(Forward)
+		}
+		if (isPressed(GLFW_KEY_S)) {
+			m_pos = m_pos - camspeed * m_front;		//(Backward)
+		}
+		if (isPressed(GLFW_KEY_A)) {
+			m_pos = m_pos - camspeed * normalize(cross(m_front, m_up));		//(Left)
+		}
+		if (isPressed(GLFW_KEY_D)) {
+			m_pos = m_pos + camspeed * normalize(cross(m_front, m_up));		//(Right)
+		}
 	}
-
-	void Camera::moveRight(const float camSpeed) {
-		m_pos = m_pos + camSpeed * normalize(cross(m_front, m_up));
-	}
-
-	void Camera::moveLeft(const float camSpeed) {
-		m_pos = m_pos - camSpeed * normalize(cross(m_front, m_up));
-	}
-
-	void Camera::increaseAngle(const float x, const float y) {
+	
+	void Camera::onMouseMotion() {
+	
 		//Increase camera's yaw and pitch angles on mouse movement
-		m_yaw += x ;
-		m_pitch += y;
+		m_yaw += getOffsetX();
+		m_pitch += getOffsetY();
 
 		//Prevent camera from looking straight up or down(messes up the lookAt matrix)
 		if (m_pitch > 89.0f)
 			m_pitch = 89.0f;
-		if(m_pitch<-89.0f)
+		if (m_pitch < -89.0f)
 			m_pitch = -89.0f;
-	}
 
-	void Camera::rotateCamera() {
 		//Update camera's front
-		Utility util = Utility::instance();
 		vector3d tempFront;
-		tempFront.m_x = cos(util.toRad(m_yaw)) * cos(util.toRad(m_pitch));
-		tempFront.m_y = sin(util.toRad(m_pitch));
-		tempFront.m_z = sin(util.toRad(m_yaw)) * cos(util.toRad(m_pitch));
-
+		tempFront.m_x = cos(toRad(m_yaw)) * cos(toRad(m_pitch));
+		tempFront.m_y = sin(toRad(m_pitch));
+		tempFront.m_z = sin(toRad(m_yaw)) * cos(toRad(m_pitch));
 		m_front = normalize(tempFront);
 	}
 }
--- Creyon/src/Render/Camera.h
+++ Creyon/src/Render/Camera.h
@@ -4,49 +4,29 @@
 #include "MathEngine/matrix4x4.h"
 #include "MathEngine/transforms.h"
 #include "Utilities/Utility.h"
+#include "Core/Entity.h"
 
 //Namespace is Creyon
 namespace Creyon {
-	
-	class Camera{	//Class to represent a camera
-		private:
-			vector3d m_pos, m_target, m_up, m_front;
-			float m_speed, m_yaw, m_pitch;
 
-		public:
-			//Initializes the camera position and target using setCamera()
-			Camera();
+	class Camera:public Entity {
+	private:
+		vector3d m_pos, m_target, m_up, m_front;
+		float m_speed, m_yaw, m_pitch;
 
-			//Get m_speed
-			float getSpeed() { return m_speed; }
-			
-			//Sets camera to view at some specific target from specified position
-			void setCamera(const vector3d& pos, const vector3d& front, const vector3d& up, const float speed);
+	public:
+		Camera();
 
-			//Moves camera forward
-			void moveForward(const float camSpeed);
+		//Generates the lookAt matrix
+		Mat44 lookAt();
 
-			//Moves camera forward
-			void moveBackward(const float camSpeed);
+		//Handles Keyboard Inputs
+		void onKey() override;
 
-			//Moves camera forward
-			void moveRight(const float camSpeed);
+		//Handles Mouse Inputs
+		void onMouseMotion() override;
 
-			//Moves camera forward
-			void moveLeft(const float camSpeed);
-
-			//Updates horizontal and vertical angle
-			void increaseAngle(const float x, const float y);
-
-			//Rotates camera according to mouse
-			void rotateCamera();
-
-			//Updates the camera target
-			void UpdateTarget();
-
-			//Generates the lookAt matrix
-			Mat44 lookAt();
 	};
-	//Reference: OglDev for camera's quaternion rotation and learnOpengl.com
+	//Reference: LearnOpengl.com
 }
 
