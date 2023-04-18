d89b3d53a6895fff2e3e25d811bcfd81b1b8d932
blades <rajatmaurya1505@gmail.com>
2023-04-18 21:56:04 +0530

    Implement Phong Lighting Model

--- Creyon/src/Render/Shaders/FragmentShader.glsl
+++ Creyon/src/Render/Shaders/FragmentShader.glsl
@@ -3,8 +3,28 @@ out vec4 FragColor;
 
 uniform vec3 lightColor;
 uniform vec3 objectColor;
+uniform vec3 lightPos;
+
+in vec3 normal;
+in vec3 FragPos;
 
 void main()
 {
-FragColor = vec4(lightColor*objectColor, 1.0);
+float ambientstrength = 0.1;
+vec3 ambient = ambientstrength * lightColor;
+
+vec3 norm = normalize(normal);
+vec3 lightDir = normalize(lightPos - FragPos);
+
+float diff = max(dot(lightDir, norm), 0.0);
+vec3 diffuse = diff * lightColor;
+
+float specularstrength = 0.5;
+vec3 viewDir = normalize(-FragPos);
+vec3 reflectDir = reflect(-lightDir, norm);
+
+float spec = pow(max(dot(viewDir, reflectDir), 0.0), 64);
+vec3 specular = specularstrength * spec * lightColor;
+
+FragColor = vec4((ambient+ diffuse + specular)*objectColor, 1.0);
 }
--- Creyon/src/Render/Shaders/VertexShader.glsl
+++ Creyon/src/Render/Shaders/VertexShader.glsl
@@ -1,11 +1,18 @@
 #version 430 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform mat3 normalmat;
+
+out vec3 normal;
+out vec3 FragPos;
 
 void main()
 {
 gl_Position= vec4(aPos, 1.0) * model* view * projection;
+normal = aNormal * normalmat;
+FragPos = vec3(vec4(aPos, 1.0) * model * view);
 }
