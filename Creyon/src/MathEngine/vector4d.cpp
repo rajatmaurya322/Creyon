123ae6e99dd1a2d5611f6eee46269bc8b07acee5
blades <rajatmaurya1505@gmail.com>
2023-04-18 21:53:15 +0530

    Rename class to Vector4d

--- Creyon/src/MathEngine/vector4d.cpp
+++ Creyon/src/MathEngine/vector4d.cpp
@@ -3,16 +3,14 @@
 //Namespace is Creyon
 namespace Creyon {
 	
-	vector4d vector4d::operator*(const Creyon::Mat44& mat) {
-		
-		return vector4d(m_x * mat.m_elems[0] + m_y * mat.m_elems[4] + m_z * mat.m_elems[8] + m_w * mat.m_elems[12],
-			m_x * mat.m_elems[1] + m_y * mat.m_elems[5] + m_z * mat.m_elems[9] + m_w * mat.m_elems[13],
-			m_x * mat.m_elems[2] + m_y * mat.m_elems[6] + m_z * mat.m_elems[10] + m_w * mat.m_elems[14],
-			m_x * mat.m_elems[3] + m_y * mat.m_elems[7] + m_z * mat.m_elems[11] + m_w * mat.m_elems[15]);
+	Vector4d Vector4d::operator*(const Creyon::Mat44& mat) {
+		return Vector4d(m_x * mat[0] + m_y * mat[4] + m_z * mat[8] + m_w * mat[12],
+						m_x * mat[1] + m_y * mat[5] + m_z * mat[9] + m_w * mat[13],
+						m_x * mat[2] + m_y * mat[6] + m_z * mat[10] + m_w *mat[14],
+						m_x * mat[3] + m_y * mat[7] + m_z * mat[11] + m_w *mat[15]);
 	}
 
-	std::ostream& operator<<(std::ostream& os, const vector4d& vec) {
-		
+	std::ostream& operator<<(std::ostream& os, const Vector4d& vec) {
 		os << "X:" << vec.m_x << "\nY:" << vec.m_y << "\nZ:" << vec.m_z << "\nW:" << vec.m_w;
 		return os;
 	}
--- Creyon/src/MathEngine/vector4d.h
+++ Creyon/src/MathEngine/vector4d.h
@@ -9,26 +9,26 @@ namespace Creyon {
 
  //Row vectors are used
     
-    class vector4d {
+    class Vector4d {
     public:		//All data members have m_ prefix
         float m_x, m_y, m_z, m_w;
 
         //Default constructor, makes a null vector with w=1
-        vector4d() :m_x{ 0.0f }, m_y{ 0.0f }, m_z{ 0.0f }, m_w{ 1.0 }
+        Vector4d() :m_x{ 0.0f }, m_y{ 0.0f }, m_z{ 0.0f }, m_w{ 1.0 }
         {}
 
         //Creates 4d vector based on given component values
-        vector4d(float x, float y, float z, float w) :m_x{ x }, m_y{ y }, m_z{ z }, m_w{ w }
+        Vector4d(float x, float y, float z, float w) :m_x{ x }, m_y{ y }, m_z{ z }, m_w{ w }
         {}
 
         //Copy constructor
-        vector4d(const vector4d& vec) :m_x{ vec.m_x }, m_y{ vec.m_y }, m_z{ vec.m_z }, m_w{ vec.m_w }
+        Vector4d(const Vector4d& vec) :m_x{ vec.m_x }, m_y{ vec.m_y }, m_z{ vec.m_z }, m_w{ vec.m_w }
         {}
         
 		//Vector-matrix multiplication; only v*mat form
-        vector4d operator*(const Creyon::Mat44& mat);
+        Vector4d operator*(const Creyon::Mat44& mat);
 
 		//Prints vector directly with cout
-        friend std::ostream& operator<<(std::ostream& os, const vector4d& vec);
+        friend std::ostream& operator<<(std::ostream& os, const Vector4d& vec);
     };
 }
