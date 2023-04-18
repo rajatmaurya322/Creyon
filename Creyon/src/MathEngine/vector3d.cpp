faa055a3e9c7223df4b41d14113b7519b8952814
blades <rajatmaurya1505@gmail.com>
2023-04-18 21:52:35 +0530

    Rename class to Vector3d

--- Creyon/src/MathEngine/vector3d.cpp
+++ Creyon/src/MathEngine/vector3d.cpp
@@ -3,71 +3,71 @@
 //Namespace is Creyon
 namespace Creyon {
 
-    vector3d vector3d::operator+(const vector3d& v)const {
+    Vector3d Vector3d::operator+(const Vector3d& v)const {
 
-        return vector3d(m_x + v.m_x, m_y + v.m_y, m_z + v.m_z);
+        return Vector3d(m_x + v.m_x, m_y + v.m_y, m_z + v.m_z);
     }
 	
-    vector3d vector3d::operator-(const vector3d& v)const {
+    Vector3d Vector3d::operator-(const Vector3d& v)const {
         
-        return vector3d(m_x - v.m_x, m_y - v.m_y, m_z - v.m_z);
+        return Vector3d(m_x - v.m_x, m_y - v.m_y, m_z - v.m_z);
     }
 
-    float vector3d::operator*(const vector3d& v)const {
+    float Vector3d::operator*(const Vector3d& v)const {
         
         return m_x * v.m_x + m_y * v.m_y + m_z * v.m_z;
     }
 
-    vector3d vector3d::operator-()const {
+    Vector3d Vector3d::operator-()const {
         
-        return vector3d(-m_x, -m_y, -m_z);
+        return Vector3d(-m_x, -m_y, -m_z);
     }
 
-    vector3d vector3d::operator*(float s)const {
+    Vector3d Vector3d::operator*(float s)const {
 		//Multiplies all components with s
-        return vector3d(m_x * s, m_y * s, m_z * s);
+        return Vector3d(m_x * s, m_y * s, m_z * s);
     }
 
-    vector3d vector3d::operator/(float s)const {
+    Vector3d Vector3d::operator/(float s)const {
         if (s == 0) {	//Can't divide by zero
             std::cout << "Vector division by 0 error";
             exit(EXIT_FAILURE);
         }
         else {
-            return vector3d(m_x / s, m_y / s, m_z / s);
+            return Vector3d(m_x / s, m_y / s, m_z / s);
         }
     }
 
-    vector3d vector3d::operator*(const Creyon::Mat33 &mat)const{
+    Vector3d Vector3d::operator*(const Creyon::Mat33 &mat)const{
         
-        return vector3d(m_x * mat.m_elems[0] + m_y * mat.m_elems[3] + m_z * mat.m_elems[6],
-                        m_x * mat.m_elems[1] + m_y * mat.m_elems[4] + m_z * mat.m_elems[7],
-                        m_x * mat.m_elems[2] + m_y * mat.m_elems[5] + m_z * mat.m_elems[8]);
+        return Vector3d(m_x * mat[0] + m_y * mat[3] + m_z * mat[6],
+                        m_x * mat[1] + m_y * mat[4] + m_z * mat[7],
+                        m_x * mat[2] + m_y * mat[5] + m_z * mat[8]);
     }
 
-    vector3d operator*(float s, const vector3d& v) {
+    Vector3d operator*(float s, const Vector3d& v) {
         return v * s;   //calls above overloaded operator *
     }
  
-    std::ostream& operator<<(std::ostream& os, const vector3d& v) {
+    std::ostream& operator<<(std::ostream& os, const Vector3d& v) {
         os << "X:" << v.m_x << " Y:" << v.m_y << " Z:" << v.m_z << "\n";
         return os;
     }
 
-    float sq_magn(const vector3d& v) { //Square of magnitude: x^2 + y^2 + z^2 
+    float sq_magn(const Vector3d& v) { //Square of magnitude: x^2 + y^2 + z^2 
         return v.m_x * v.m_x + v.m_y * v.m_y + v.m_z * v.m_z;
     }
 
-    float magn(const vector3d& v) {
+    float magn(const Vector3d& v) {
         return sqrt(sq_magn(v));  // Vector magnitude = sqrt(x^2 + y^2 + z^2)
     }
 	
-	vector3d normalize(const vector3d& v) {
+	Vector3d normalize(const Vector3d& v) {
 		//Returns a new normalized vector
 		return v / magn(v);
 	}
 
-    vector3d cross(const vector3d& v1, const vector3d& v2) {
-        return vector3d(v1.m_y * v2.m_z - v1.m_z * v2.m_y, v1.m_z * v2.m_x - v1.m_x * v2.m_z, v1.m_x * v2.m_y - v1.m_y * v2.m_x);
+    Vector3d cross(const Vector3d& v1, const Vector3d& v2) {
+        return Vector3d(v1.m_y * v2.m_z - v1.m_z * v2.m_y, v1.m_z * v2.m_x - v1.m_x * v2.m_z, v1.m_x * v2.m_y - v1.m_y * v2.m_x);
     }
 }
--- Creyon/src/MathEngine/vector3d.h
+++ Creyon/src/MathEngine/vector3d.h
@@ -9,73 +9,73 @@
 //Row vectors are used
 namespace Creyon {
 
-    class vector3d{
+    class Vector3d{
     public:		//All data members of a class have m_ prefix
         float m_x, m_y, m_z;
 
         //Default constructor, creates a null vector
-        vector3d() :m_x{ 0.0f }, m_y{ 0.0f }, m_z{ 0.0f }
+        Vector3d() :m_x{ 0.0f }, m_y{ 0.0f }, m_z{ 0.0f }
         {}
 
         //Creates a vector based on given components
-        vector3d(float x, float y, float z) :m_x{ x }, m_y{ y }, m_z{ z }
+        Vector3d(float x, float y, float z) :m_x{ x }, m_y{ y }, m_z{ z }
         {}
 
         //Copy constructor
-        vector3d(const vector3d& v) :m_x{ v.m_x }, m_y{ v.m_y }, m_z{ v.m_z }
+        Vector3d(const Vector3d& v) :m_x{ v.m_x }, m_y{ v.m_y }, m_z{ v.m_z }
         {}
 
 		
 		//Add two vectors(respective x,y,z components)
-        vector3d operator+(const vector3d& v)const;
+        Vector3d operator+(const Vector3d& v)const;
 		
 		//Subtract two vectors(respective x,y,z components)
-        vector3d operator-(const vector3d& v)const;
+        Vector3d operator-(const Vector3d& v)const;
 		
 		//Dot product of two vectors
-        float operator*(const vector3d& v)const;
+        float operator*(const Vector3d& v)const;
         
         //Vector-matrix multiplication; only v*mat form
-        vector3d operator*(const Creyon::Mat33 &mat)const;
+        Vector3d operator*(const Creyon::Mat33 &mat)const;
 
         //unary minus, returns a vector with negated components
-        vector3d operator-()const;
+        Vector3d operator-()const;
 		
 		//Scalar multiplication of vector with s : v*s form
-        vector3d operator*(float s)const;
+        Vector3d operator*(float s)const;
 		
 		// Scalar division of vector with s : v / s
-        vector3d operator/(float s)const;
+        Vector3d operator/(float s)const;
 
 		//Checks if two vectors are equal
-        bool operator==(const vector3d& v)const {
+        bool operator==(const Vector3d& v)const {
             return m_x == v.m_x && m_y == v.m_y && m_z == v.m_z;
         }
 
 		//Checks if two vectors are not equal
-        bool operator!=(const vector3d& v)const {
+        bool operator!=(const Vector3d& v)const {
             return m_x != v.m_x || m_y != v.m_y || m_z != v.m_z;
         }
         
 		//Scalar multiplication of vector with s: s*v form
-        friend vector3d operator*(float s, const vector3d& v);
+        friend Vector3d operator*(float s, const Vector3d& v);
 
         //Prints vector directly with cout
-        friend std::ostream& operator<<(std::ostream& os, const vector3d& v);
+        friend std::ostream& operator<<(std::ostream& os, const Vector3d& v);
     };
 
     //Non member Functions
 	
     //Returns square of the magnitude of the vector
-    float sq_magn(const vector3d& v);
+    float sq_magn(const Vector3d& v);
 
     //Returns magnitude of the vector
-    float magn(const vector3d& v);
+    float magn(const Vector3d& v);
 
 	//Normalize a vector
-	vector3d normalize(const vector3d& v);
+	Vector3d normalize(const Vector3d& v);
 
     //Cross product of two vectors
-    vector3d cross(const vector3d& a, const vector3d& b);
+    Vector3d cross(const Vector3d& a, const Vector3d& b);
 
 }
