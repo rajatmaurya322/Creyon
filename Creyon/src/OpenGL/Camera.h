b255c26ff4c2222200b61eca283e6f901ea54be3
blades <rajatmaurya1505@gmail.com>
2024-08-15 11:57:18 +0530

    update camera lookAt()

--- Creyon/src/OpenGL/Camera.h
+++ Creyon/src/OpenGL/Camera.h
@@ -4,12 +4,13 @@
 #include "Math/Vector3.h"
 #include "Math/Matrix4x4.h"
 #include "Window/Subscriber.h"
+#include "Math/Transforms.h"
 
 namespace Creyon {
 	
 	class Camera : public Subscriber{
 	public:
-		Vector3 m_position, m_target, m_worldUp, m_front;
+		Vector3 m_position, m_front, m_right;
 		float m_speed, m_yaw, m_pitch, m_fieldOfView;
 		
 		void updateKeyboard(const std::unordered_map<int,bool>& keyStates, float deltaTime)override;
@@ -17,6 +18,8 @@ namespace Creyon {
 		void updateCursor(float offsetX, float offsetY)override;
 
 		void updateMouseScroll(float offsetX, float offsetY)override;
+		
+		// Reference : https://stannum.io/blog/0UaG8R
 		
 		//returns the lookAt matrix
 		Mat44 lookAt();
