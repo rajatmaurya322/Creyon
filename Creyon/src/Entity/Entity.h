8c579e5800715357143d6d23edf85d34e115e18f
blades <rajatmaurya1505@gmail.com>
2024-10-13 17:36:32 +0530

    experimental folder Entity

---
+++ Creyon/src/Entity/Entity.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Creyon {
+
+	class Entity {
+	private:
+		/*
+		Input* p_inputComponent; <-- Input interface(abstract class) pointer stores the implemented class object
+		
+		*/
+	public:
+		Entity();
+
+		/*
+		update();
+		*/
+
+	};
+}
