7c3cea5ec8bc6e7eaa01fb96d9c468c6b70487bd
blades <rajatmaurya1505@gmail.com>
2025-01-05 18:53:24 +0530

    remove model processing functions based on assimp

--- Creyon/src/Model/Model.h
+++ Creyon/src/Model/Model.h
@@ -1,9 +1,6 @@
 #pragma once
 
 #include "Mesh.h"
-#include "assimp/Importer.hpp"
-#include "assimp/scene.h"
-#include "assimp/postprocess.h"
 
 namespace Creyon {
 
@@ -13,15 +10,9 @@ namespace Creyon {
 		std::string m_directory;
 		
 		void loadModel(const std::filesystem::path& path);
-		
-		void processNode(aiNode* node, const aiScene* scene);
-		
-		Mesh processMesh(aiMesh* mesh, const aiScene* scene);
-		
-		//std::vector<Texture> loadMaterialTextures(aiMaterial* material, aiTextureType type, Texture::Type typeName);
 
 	public:
-		//Takes relative path to model from Creyon/src and loads it
+
 		Model(const std::filesystem::path& modelPath);
 		
 		void Draw(ShaderProgram& shader);
